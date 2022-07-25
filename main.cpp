3112fd767f3e218df186ee8165169dd0ba00a4f2
ANDREJ <and423@inbox.lv>
2022-07-25 13:00:31 +0200

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿
+#include "Task30__2.h"
+#include <cpr/cpr.h>
+
+using namespace std;
+
+int main()
+{
+  std::string url = "http://httpbin.org/html";
+  cpr::Response r = cpr::Get(cpr::Url(url), cpr::Header({ {"accept", "text/html"} }));
+  size_t found =  r.text.find("<h1>");
+  size_t found2 = r.text.find("</h1>");
+  std::string str(r.text , found + 4, found2 - found - 4);
+  std::cout << str << std::endl;
+	return 0;
+}
